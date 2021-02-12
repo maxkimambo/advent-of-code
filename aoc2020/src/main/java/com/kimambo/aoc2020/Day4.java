c7b6020633cb0070053cb39d7eb8f48156ec9c5a
Max Kimambo <max@kimambo.de>
2021-02-12 20:32:01 +0100

    day 4 part 2

--- aoc2020/src/main/java/com/kimambo/aoc2020/Day4.java
+++ aoc2020/src/main/java/com/kimambo/aoc2020/Day4.java
@@ -19,7 +19,7 @@ public class Day4 {
     private List<HashMap<String, String>> validPassports = new ArrayList<>();
 
     public Day4(ResourceFileReader fr) {
-       
+
         reader = fr;
 
         try {
@@ -36,8 +36,8 @@ public class Day4 {
         List<HashMap<String, String>> passportData = new ArrayList<>();
         boolean next = false;
 
-        for(int i=0; i < contents.size(); i++){
-            String line = contents.get(i); 
+        for (int i = 0; i < contents.size(); i++) {
+            String line = contents.get(i);
             if (next) {
                 passportDataSet = new ArrayList<>();
                 next = false;
@@ -49,8 +49,8 @@ public class Day4 {
                 passportData.add(extractPassportData(passportDataSet));
             }
 
-            // add remaining items if file doesnt end with an empty line 
-            if (i == (contents.size() -1)) {
+            // add remaining items if file doesnt end with an empty line
+            if (i == (contents.size() - 1)) {
                 passportData.add(extractPassportData(passportDataSet));
             }
         }
@@ -93,6 +93,102 @@ public class Day4 {
         return validPassportCount;
     }
 
+    private List<HashMap<String, String>> validateFieldPresence(List<HashMap<String, String>> passports,
+            HashMap<String, Boolean> rules) {
+
+        List<HashMap<String, String>> validPassports = new ArrayList<>();
+
+        for (HashMap<String, String> pass : passports) {
+
+            if (isValid(pass, rules)) {
+                validPassports.add(pass);
+            }
+        }
+        return validPassports;
+    }
+
+    private boolean validateHeight(String hgt) {
+
+        boolean validFormat = hgt.matches("^\\d+cm|in");
+
+        if (!validFormat) {
+            return false;
+        }
+
+        try {
+
+            String[] heightValue = hgt.split("cm|in");
+            int height = Integer.parseInt(heightValue[0]);
+            String[] unitValue = hgt.split("\\d+");
+            String unit = unitValue[1];
+
+            if (unit.equals("cm") && (height >= 150 && height <= 193)) {
+                return true;
+            }
+
+            if (unit.equals("in") && (height >= 59 && height <= 76)) {
+                return true;
+            }
+
+        } catch (IndexOutOfBoundsException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return false;
+    }
+
+    private int Solve2(List<HashMap<String, String>> passports, HashMap<String, Boolean> rules) {
+
+        List<HashMap<String, String>> validPassports = new ArrayList<>();
+        // List<HashMap<String, String>> invalidPassports = new ArrayList<>();
+        // check for presence of required fields
+        List<HashMap<String, String>> passportsWithRequiredFields = validateFieldPresence(passports, rules);
+        // check if the fields conform to the rules
+        
+        for (HashMap<String, String> pass : passportsWithRequiredFields) {
+            HashMap<String, Boolean> validationResults = new HashMap<>();
+
+            validationResults.put("hgt", validateHeight(pass.get("hgt")));
+
+            validationResults.put("byr", validateYearRange(1920, 2002, pass.get("byr")));
+
+            validationResults.put("iyr", validateYearRange(2010, 2020, pass.get("iyr")));
+
+            validationResults.put("eyr", validateYearRange(2020, 2030, pass.get("eyr")));
+
+            validationResults.put("hcl", itemValidator(pass.get("hcl"), "^#[0-9a-z]{6}"));
+
+            validationResults.put("ecl", itemValidator(pass.get("ecl"), "amb|blu|brn|gry|grn|hzl|oth"));
+
+            validationResults.put("pid", itemValidator(pass.get("pid"), "\\d{9,9}"));
+
+            // if all are valid perform count.
+            if (!validationResults.containsValue(false)) {
+                validPassports.add(pass);
+            }
+            // }else{
+            //     invalidPassports.add(pass); 
+            // }
+        }
+
+        return validPassports.size();
+    }
+
+    private boolean validateYearRange(int low, int high, String yearValue) {
+        try {
+            int year = Integer.parseInt(yearValue);
+            return year >= low && year <= high;
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private boolean itemValidator(String input, String regex) {
+        return input.matches(regex);
+    }
+
     public void Solve() {
 
         HashMap<String, Boolean> validationRules = new HashMap<>();
@@ -108,5 +204,9 @@ public class Day4 {
 
         int result = Solve1(taskInput, validationRules);
         System.out.println("Valid passports => " + result);
+
+        int result2 = Solve2(taskInput, validationRules);
+
+        System.out.println("Valid passports with valid values => " + result2);
     }
 }
